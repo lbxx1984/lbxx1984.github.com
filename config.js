79d83ad1c053ee94aee0b725937644763af3464f
lbxx1984 <279641976@qq.com>
2014-07-03 12:55:00 +0800

    modify config.js
    rename 'examples' to 'example'

--- config.js
+++ config.js
@@ -11,14 +11,14 @@ var config={
 	welcome:"G3-team 致力于web、ios、android应用开发。基于HTML5、CSS3、Ajax、jQuery、Flex等流行技术是我们开发web应用的基础；保证IE、Firefox、Chrome、Safari、Opera等浏览器兼容性是我们的开发前提；扩充到ios、android、wm等移动平台是我们的承诺；提高应用性能、改善应用体验是我们的最终目的。同时，LHTSOFT热衷于3D-online页游开发，欢迎各界精英参与合作。",
 	newapp:["TcEditor4，同TcEditor3相比，这是一款真正意义的3D模型编辑器。基于THREE.js版本63开发，可任意修改几何体内骨骼点位置，以实现复杂曲面创建。未来，我们将加入更强大快捷的辅助设计器，用于模型编辑；并增加模型导出功能，来实现与其他应用的对接。<hr>","TcFrame3，TcFrame3使用jQuery1.9核心，是一款以json为数据源的jQuery-UI框架。集成众多源插件，并对这些插件进行重新书写，以提高这些插件的普适性、易用性。同时，我们将开放皮肤引擎，为前端工程师们提供最快捷的开发工具。"],	
 	slider:[
-		{img:"s08.png",label:"TcEditor4 demo",url:exampleURL+"examples/tceditor4/"},
-		{img:"s07.png",label:"TcFrame3 demo",url:exampleURL+"examples/tcframe3/"},
-		{img:"s06.jpg",label:"TcFrame2.0",url:exampleURL+"examples/tcframe2/"},
-		{img:"s05.jpg",label:"TCmodelEditor3",url:exampleURL+"examples/TCmodelEditor3/"},
-		{img:"s04.jpg",label:"DTSK1.0",url:exampleURL+"examples/dtsk/"},
-		{img:"s03.jpg",label:"L3DmodelEditor1.1",url:exampleURL+"examples/L3DmodelEditor/"},
-		{img:"s02.jpg",label:"Flex TextEditor beta1",url:exampleURL+"examples/texteditor/"},
-		{img:"s01.jpg",label:"epainter1.0",url:exampleURL+"examples/epainter/"}
+		{img:"s08.png",label:"TcEditor4 demo",url:exampleURL+"example/tceditor4/"},
+		{img:"s07.png",label:"TcFrame3 demo",url:exampleURL+"example/tcframe3/"},
+		{img:"s06.jpg",label:"TcFrame2.0",url:exampleURL+"example/tcframe2/"},
+		{img:"s05.jpg",label:"TCmodelEditor3",url:exampleURL+"example/TCmodelEditor3/"},
+		{img:"s04.jpg",label:"DTSK1.0",url:exampleURL+"example/dtsk/"},
+		{img:"s03.jpg",label:"L3DmodelEditor1.1",url:exampleURL+"example/L3DmodelEditor/"},
+		{img:"s02.jpg",label:"Flex TextEditor beta1",url:exampleURL+"example/texteditor/"},
+		{img:"s01.jpg",label:"epainter1.0",url:exampleURL+"example/epainter/"}
 	],
 	about:"<a target='_blank' href='http://weibo.com/lbxx1984'>http://weibo.com/lbxx1984</a><br><a href='http://weibo.com/dfone' target='_blank'>http://weibo.com/dfone</a><a href='http://weibo.com/chuanqi' target='_blank'><br>http://weibo.com/chuanqi</a><br><a target='_blank' href='http://www.fancyber.com/'>fancyber.com</a><br><a target='_blank' href='http://www.mrxiao.com/'>mrxiao.com</a>",
 	team:[
@@ -27,12 +27,12 @@ var config={
 		{url:"003.jpg",intro:'vivo，大神一枚，创意无限，现在主要做ios方面开发。<hr>动手能力：<span>★★★★★</span><br>创新能力：<span>★★★★★</span><br>逆向工程：<span>★★★☆</span><br>技术探索：<span>★★★★★</span><br>产品架构：<span>★★★★★</span>'}
 	],	
 	applications:[
-		{label:"TcFrame2.0",url:exampleURL+"examples/tcframe2/",introduction:"一款非常优秀的轻量级javascript嵌入式框架，定义了js面向对象扩展，不足200KB的基础包中，重新定义了丰富的可视化组件及基础特效组件，整体结构清晰明了，文档齐全，API丰富。"
-		},{label:"TCmodelEditor3",url:exampleURL+"examples/TCmodelEditor3/",introduction:"一款应用WebGL技术制作的online-3D模型编辑器，使用开源THREE.js引擎。相对L3DmodelEditor，有更强的性能。但要求浏览器支持WebGL，建议使用Chrome15+浏览器。TCmodelEditor同时也是www.tctctc.com使用的编辑器。"
-		},{label:"DTSK1.0",url:exampleURL+"examples/dtsk/",introduction:"基于FlexSDK4.5 spark主题的轻量级WebOS框架，使用极方便的DTSK语言，轻松生成图标、窗体，并支持自定义application载入。"
-		},{label:"L3DmodelEditor1.1",url:exampleURL+"examples/L3DmodelEditor/",introduction:"一款应用Alternativa3D 7.5引擎制作的flash-3D模型制作工具。由于A3D 8后采用了不同架构，这个应用只能当作一种展示。在网页上实现3D不是件容易的事，实现3D模型的编辑就更有难度。所以这个编辑器只是抛砖引玉，它能实现简单的3D模型编辑，提供了几种常用的材质。"
-		},{label:"Flex TextEditor beta1",url:exampleURL+"examples/texteditor/",introduction:"制作工具FlashBuilder4.5，SDK4.5，应用textlayoutAPI，基本实现了常用的富文本编辑功能，适用于对排版要求不高的flex应用。"
-		},{label:"epainter1.0",url:exampleURL+"examples/epainter/",introduction:"一款用flex3开发的online画图工具。几乎使用了flash.graphy类的所有功能。用sdk4.5重新编译后，出现了很多不兼容问题，之前还能上传打开图片，支持保存下载jpg、png文件。鉴于这个应用是sdk3做的，很多地方过时了，所以没修复bug，有兴趣着可以与工作室联系，获取代码支持。仅供娱乐用。"
+		{label:"TcFrame2.0",url:exampleURL+"example/tcframe2/",introduction:"一款非常优秀的轻量级javascript嵌入式框架，定义了js面向对象扩展，不足200KB的基础包中，重新定义了丰富的可视化组件及基础特效组件，整体结构清晰明了，文档齐全，API丰富。"
+		},{label:"TCmodelEditor3",url:exampleURL+"example/TCmodelEditor3/",introduction:"一款应用WebGL技术制作的online-3D模型编辑器，使用开源THREE.js引擎。相对L3DmodelEditor，有更强的性能。但要求浏览器支持WebGL，建议使用Chrome15+浏览器。TCmodelEditor同时也是www.tctctc.com使用的编辑器。"
+		},{label:"DTSK1.0",url:exampleURL+"example/dtsk/",introduction:"基于FlexSDK4.5 spark主题的轻量级WebOS框架，使用极方便的DTSK语言，轻松生成图标、窗体，并支持自定义application载入。"
+		},{label:"L3DmodelEditor1.1",url:exampleURL+"example/L3DmodelEditor/",introduction:"一款应用Alternativa3D 7.5引擎制作的flash-3D模型制作工具。由于A3D 8后采用了不同架构，这个应用只能当作一种展示。在网页上实现3D不是件容易的事，实现3D模型的编辑就更有难度。所以这个编辑器只是抛砖引玉，它能实现简单的3D模型编辑，提供了几种常用的材质。"
+		},{label:"Flex TextEditor beta1",url:exampleURL+"example/texteditor/",introduction:"制作工具FlashBuilder4.5，SDK4.5，应用textlayoutAPI，基本实现了常用的富文本编辑功能，适用于对排版要求不高的flex应用。"
+		},{label:"epainter1.0",url:exampleURL+"example/epainter/",introduction:"一款用flex3开发的online画图工具。几乎使用了flash.graphy类的所有功能。用sdk4.5重新编译后，出现了很多不兼容问题，之前还能上传打开图片，支持保存下载jpg、png文件。鉴于这个应用是sdk3做的，很多地方过时了，所以没修复bug，有兴趣着可以与工作室联系，获取代码支持。仅供娱乐用。"
 		}
 	]	
 }
