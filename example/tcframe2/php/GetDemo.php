0e27f3b3abe20d35b5503d8072f3441fadc52088
lbxx1984 <279641976@qq.com>
2015-11-28 22:45:51 +0800

    remove tceditor4， remove tcframe2 docs

--- example/tcframe2/php/GetDemo.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$id=$_GET['id'];
-	$param=$_GET['param'];
-	
-	
-	print_r('From server by GET:'.$id.','.$param);
-?>
--- example/tcframe2/php/PostDemo.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$id=$_POST['id'];
-	$param=$_POST['param'];
-	print_r('From server by POST:'.$id.','.$param);
-?>
